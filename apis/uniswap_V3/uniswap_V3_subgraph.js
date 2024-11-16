e78ef4627a51a794fbf9da678ca28ff7ba4d8465
Al-Crypto477 <121586022+Al-Crypto477@users.noreply.github.com>
2024-11-16 14:00:39 -0600

    changed adress for the uniswap v3 graph endpoint

--- apis/uniswap_V3/uniswap_V3_subgraph.js
+++ apis/uniswap_V3/uniswap_V3_subgraph.js
@@ -3,7 +3,9 @@ const axios = require('axios');
 
 async function uniswapSubgraph_V3_Api(query) {
   try {
-    const url = 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3';
+    const url = 'https://gateway.thegraph.com/api/ad1435e8721f29bb5c77715a690b9fca/subgraphs/id/5zvR82QoaXYFyDEKLZ9t6v9adgnptxYpKpSbxtgVENFV';
+    
+    //'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3';
     const { data } = await axios.post(url, query);
 
     return data.data;
